700c595a3ad0a9d4bc51db3db7bf4ecfcae846c8
Jerome DH <jerome.dehertogh@gmail.com>
2018-11-06 16:50:48 +0100

    Premier commit

---
+++ js/script.js
@@ -0,0 +1,127 @@
+jQuery(function($){
+	$.datepicker.regional['fr'] = {
+		closeText: 'Fermer',
+		prevText: '&#x3c;Préc',
+		nextText: 'Suiv&#x3e;',
+		currentText: 'Aujourd\'hui',
+		monthNames: ['Janvier','Fevrier','Mars','Avril','Mai','Juin',
+		'Juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+		monthNamesShort: ['Jan','Fev','Mar','Avr','Mai','Jun',
+		'Jul','Aou','Sep','Oct','Nov','Dec'],
+		dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+		dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+		dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+		weekHeader: 'Sm',
+		dateFormat: 'DD, d MM, yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: '',
+		minDate: 0,
+		maxDate: '+12M +0D',
+		numberOfMonths: 1,
+		showButtonPanel: true,
+		showAnim: 'fold',
+		showOn: "button",
+		buttonImage: "../css/images/calendar.gif",
+		buttonImageOnly: true,
+		buttonText: "Select date"
+		};
+	$.datepicker.setDefaults($.datepicker.regional['fr']);
+});
+
+$(function(){
+	
+    	$( "#datepicker" ).datepicker(); //datepicker
+	
+		var availableTags = [
+		  "ActionScript",
+		  "AppleScript",
+		  "Asp",
+		  "BASIC",
+		  "C",
+		  "C++",
+		  "Clojure",
+		  "COBOL",
+		  "ColdFusion",
+		  "Erlang",
+		  "Fortran",
+		  "Groovy",
+		  "Haskell",
+		  "Java",
+		  "JavaScript",
+		  "Lisp",
+		  "Perl",
+		  "PHP",
+		  "Python",
+		  "Ruby",
+		  "Scala",
+		  "Scheme"
+		];//availabletags
+	
+    /*$( "#tags" ).autocomplete({
+      source: availableTags,
+		minLenght: 2,
+    });*/
+	
+	$( "#tags" ).autocomplete({
+  		source: function( request, response ) {
+          var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+          response( $.grep( availableTags, function( item ){
+              return matcher.test( item );
+          }) );
+      	}
+	});//Autocomplete
+	
+	$.ajax({
+		
+		url:'../json/cities.json',
+		method: "GET",
+		dataType: "json",
+		success:function(monObjet){
+			
+			//console.log(monObjet.length);
+			
+			var i = 0;
+			
+			var villes = [];
+			
+			for(i=0; i<monObjet.length; i++){
+				
+				//console.log(monObjet[i].name);
+				
+				var obj = {};
+				
+				obj["value"] = monObjet[i].zip;
+				obj["label"] = monObjet[i].zip+ " " +monObjet[i].name;
+				obj["ville"] = monObjet[i].name;
+				
+				villes.push(obj); //Ajoute des valeurs dans l'array "villes".
+				
+			}//for
+			
+			console.log(villes);
+			
+			$("#cp").autocomplete({
+				
+				source: function( request, response ) {
+				  var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+				  response( $.grep( villes, function( item ){
+					  return matcher.test( item.label );
+				  }) );
+      			},
+				
+				select: function(event, ui){
+					
+					$("#ville").val(ui.item.ville);
+					
+				}
+
+				
+			});//autocomplete
+			
+		}//success
+		
+	});//ajax
+	
+}); //ready
